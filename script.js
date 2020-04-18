fa5aad2c966c8742b9eacfcc289377502c90dede
Napoleon Vuong <napolo101@gmail.com>
2020-04-18 16:17:25 -0700

    Reinitialized all character values to randomize automatically within functions

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lCase = "abcdefghijklmnopqrstuvwxyz";
-var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var nums = "0123456789";
-var chars = "!@#$%^&*()";
+// var lCase = "abcdefghijklmnopqrstuvwxyz";
+// var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var nums = "0123456789";
+// var chars = "!@#$%^&*()";
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  var symbols = "!@#$%^&*(){}[]=<>/,.'";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+var lCaseTrue = true;
+var uCaseTrue = true;
+var numsTrue = true;
+var charsTrue = true;
 
 // Write password to the #password input
 function writePassword() {
+  var userLength = prompt("How long would you like your password to be? (8-128)");
+  if (userLength > 128 || userLength < 8) {
+    alert("Please pick a valid number range.");
+  }
+  lCaseTrue = confirm("Would you like lower case letters in your password?");
+  uCaseTrue = confirm("Would you like uppercase letters in your password?");
+  numsTrue = confirm("Would you like numbers in your password?");
+  charsTrue = confirm("Would you like special characters in your password?");
+
+  function generatePassword() {
+    if (lCaseTrue) {
+      document.getElementById("password").innerHTML = "Yahoo New Password!";
+
+    }
+  }
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
 
 
 
