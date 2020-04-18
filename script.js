ab5f0e3e958316f0c77959b991eeb7c8f0307213
Napoleon Vuong <napolo101@gmail.com>
2020-04-18 14:40:02 -0700

    first commit with initialized character choices

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lCase = "abcdefghijklmnopqrstuvwxyz";
+var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var nums = "0123456789";
+var chars = "!@#$%^&*()";
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
+
+
+
