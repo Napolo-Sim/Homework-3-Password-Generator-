8c3136c253ca5361c06b4f60755dfa38d4634243
Napoleon Vuong <napolo101@gmail.com>
2020-04-18 18:45:52 -0700

    Final rough draft of the homework code with comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//All randomised character functions
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
